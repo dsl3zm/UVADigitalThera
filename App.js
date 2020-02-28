1c8a522df981cbb893af5225e022be431e00aeac
Daniel Lysinger <dsl3zm@virginia.edu>
2020-02-28 10:05:21 -0500

    user location added

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    location: {},
+    errorMessage: ''
+  }
+  _getLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if(status !== "granted") {
+      console.log('PERMISSION NOT GRANTED!');
+      
+      this.setState({
+        errorMessage: 'PERMISSION NOT GRANTED'
+      })
+
+    }
+    
+    const userLocation = await Location.getCurrentPositionAsync();
+    this.setState({
+      location
+    })
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container}>          
+
         <MapView style={styles.mapStyle} 
           initialRegion={{
             latitude: 38.0289396,
@@ -13,12 +37,13 @@ export default class App extends React.Component {
             latitudeDelta: 0.0,
             longitudeDelta: 0.01,
           }} 
+          showsUserLocation={true}
           >
           <MapView.Marker
             coordinate={{latitude: 38.0289396,
             longitude: -78.5210302}}
             title={"Runk Dining Hall"}
-            description={"good food"}
+            description={"Good food"}
          />
           </MapView>
         
