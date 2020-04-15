3900bb036d11d52639cba143dfe671e94de0fe7d
Alex Grace <alexgrace@Alexs-MacBook-Pro.local>
2020-04-14 20:33:06 -0400

    breathing page

--- screens/Breathe.js
+++ screens/Breathe.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Switch, Text, Image } from 'react-native';
-import { Constants } from 'expo';
+import { View, StyleSheet, Switch, Text, Image} from 'react-native';
 import NavBar from './NavBar'
+import { Audio } from 'expo-av';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Breathe(props){
 
+const soundObject = new Audio.Sound();
+playSound = async () => {
+  await soundObject.loadAsync(require('../assets/music1.mp3'));
+  soundObject.playAsync();
+}
+// try {
+//   await soundObject.loadAsync(require('./assets/music1.mp3'));
+//   await soundObject.playAsync();
+//   // Your sound is playing!
+// } catch (error) {
+//   // An error occurred!
+// }
+
   return(
+    
     <View style={{ width:'100%',height:'100%' }}>
         <NavBar title='Breathing' navigation = {props.navigation }/>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image source={{uri: 'https://media.giphy.com/media/krP2NRkLqnKEg/giphy.gif'}} style={styles.gif} />
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <TouchableOpacity>
+            <Ionicons name="md-play" size={60} color="black" style={styles.button} />
+          </TouchableOpacity>
+          <Image source={{uri: 'https://media.giphy.com/media/krP2NRkLqnKEg/giphy.gif'}} style={styles.gif} />
         </View>
     </View>
     )
@@ -22,8 +41,12 @@ const styles = StyleSheet.create({
   gif:{
     width: '80%',
     height: '60%',
+  },
+  button: {
+    paddingBottom: 60
 
   }
+
 
 
 
